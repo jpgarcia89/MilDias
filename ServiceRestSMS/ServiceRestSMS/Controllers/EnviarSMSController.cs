0a92c0b7514e47c2894c802df7f89c4e68e7d2a6
juanmanuelariza <juanma.ariza23@gmail.com>
2017-11-06 09:01:42 -0300

    Sobrecarga metodo EnviarSMS

--- ServiceRestSMS/ServiceRestSMS/Controllers/EnviarSMSController.cs
+++ ServiceRestSMS/ServiceRestSMS/Controllers/EnviarSMSController.cs
@@ -16,75 +16,91 @@ namespace ServiceRestSMS.Controllers
 {
     public class EnviarSMSController : ApiController
     {
-        //[HttpPost]
-        //public HttpResponseMessage EnviarSMS(HttpRequestMessage Mensaje)
-        //{
-        //    ProgMilDiasEntities db = new ProgMilDiasEntities();
-        //    HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://64.76.120.14:6064/minsaludsanjuan");
-        //    try
-        //    {
-        //        string responseText = "";
-        //        string xmlString = Mensaje.Content.ReadAsStringAsync().Result;
-        //        request.ContentType = "text/xml";
-        //        request.Method = "POST";
-        //        byte[] postBytes = Encoding.UTF8.GetBytes(xmlString);
-        //        request.ContentLength = postBytes.Length;
-        //        Stream requestStream = request.GetRequestStream();
-        //        requestStream.Write(postBytes, 0, postBytes.Length);
-        //        requestStream.Close();
-        //        using (var response = (HttpWebResponse)request.GetResponse())
-        //        {
-        //            var encoding = ASCIIEncoding.ASCII;
-        //            using (var reader = new StreamReader(response.GetResponseStream(), encoding))
-        //            {
-        //                responseText = reader.ReadToEnd();
-        //            }
-        //            logMensajeRecibido log = new logMensajeRecibido();
-        //            log.Fecha = DateTime.Now;
-        //            log.Mensaje = responseText;
-        //            log.Origen = "EnviarSMS";
-        //            log.Procesado = false;
-        //            db.logMensajeRecibido.Add(log);
-        //            db.SaveChanges();                    
-        //            return new HttpResponseMessage(HttpStatusCode.OK);
-        //        }
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        request.Abort();
-        //        logSMSError logError = new logSMSError();
-        //        logError.Fecha = DateTime.Now;
-        //        logError.Mensaje = e.Message;
-        //        logError.Origen = "EnviarSMS";
-        //        db.logSMSError.Add(logError);
-        //        db.SaveChanges();
-        //        return new HttpResponseMessage(HttpStatusCode.InternalServerError);
-        //    }
-        //}
-
         [HttpPost]
-        public IHttpActionResult EnviarSM(SMS ParamSMS)
+        public IHttpActionResult EnviarSM(SMS ArgSMS)
         {
             MilDiasEntities db = new MilDiasEntities();
+            var embarazada = from e in db.Embarazada
+                                join i in db.Inscripcion on e.ID equals i.ID_EMBARAZADA
+                                select new
+                                {
+                                    telefono = e.TELEFONO,
+                                    carrier = e.Empresa.Carrier
+                                };
+            if (embarazada.ToList().Count > 0)
+            {
+                return Json(EnviarSM(ArgSMS.Mensaje, embarazada.First().carrier, embarazada.First().telefono, ArgSMS.Es_Control, ArgSMS.ID_Instancia));
+            }
+            else
+            {
+                return Json(false);
+            }
+        }
+
+        public bool EnviarSM(string ArgMensaje, string ArgCarrier, string ArgTelefono, bool ArgEsControl, string ArgInstancia)
+        {
+            MilDiasEntities db = new MilDiasEntities();
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://64.76.120.14:6064/minsaludsanjuan");
             try
             {
-                var embarazada = from e in db.Embarazada
-                                 join i in db.Inscripcion on e.ID equals i.ID_EMBARAZADA
-                                 select new
-                                 {
-                                     telefono = e.TELEFONO,
-                                     carrier = e.Empresa.Carrier
-                                 };
+                string responseText = "";
+                request.ContentType = "text/xml";
+                request.Method = "POST";
 
+                MTRequest MT = new MTRequest();
+                MT.Proveedor.Id = "minsaludsanjuan";
+                MT.Proveedor.Password = "mgs4ludsju4n";
+                MT.Servicio.Id = ArgCarrier;
+                MT.Servicio.ContentType = "0";
+                MT.Telefono.Msisdn = ArgTelefono;
+                MT.Contenido.Text = ArgMensaje;
 
-                if (embarazada.ToList().Count > 0)
+                var body = new StringWriter();
+                var serializerXML = new XmlSerializer(typeof(MTRequest));
+                serializerXML.Serialize(body, this);
+
+                byte[] postBytes = Encoding.UTF8.GetBytes(body.ToString());
+                request.ContentLength = postBytes.Length;
+                Stream requestStream = request.GetRequestStream();
+                requestStream.Write(postBytes, 0, postBytes.Length);
+                requestStream.Close();
+                using (var response = (HttpWebResponse)request.GetResponse())
                 {
+                    var encoding = ASCIIEncoding.ASCII;
+                    using (var reader = new StreamReader(response.GetResponseStream(), encoding))
+                    {
+                        responseText = reader.ReadToEnd();
+                    }
+
+                    LogMensaje log = new LogMensaje();
+                    log.FECHA = DateTime.Now;
+                    log.MENSAJE = responseText;
+                    if (ArgEsControl == true)
+                    {
+                        log.ID_TIPOMENSAJE = 4;
+
+                        var deserializerXML = new XmlSerializer(typeof(MTResponse));
+                        MTResponse MTResp = new MTResponse();
+                        using (TextReader readerXML = new StringReader(responseText))
+                        {
+                            MTResp = (MTResponse)deserializerXML.Deserialize(readerXML);
+                        }
+                        LogMensajeControl logControl = new LogMensajeControl();
+                        logControl.FECHA = DateTime.Now;
+                        logControl.ID_INSTANCIA = ArgInstancia;
+                        logControl.ID_TRANSACCION = int.Parse(MTResp.Transaccion.IdTran);
+                        logControl.ID_RESPUESTA = 3; //Por defecto ponemos que no contesto
+                        db.LogMensajeControl.Add(logControl);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        log.ID_TIPOMENSAJE = 1;
+                    }
+                    db.LogMensaje.Add(log);
+                    db.SaveChanges();
                 }
-                else
-                {
-                    return Json(false);
-                }
-          
+                return true;
             }
             catch (Exception e)
             {
@@ -92,74 +108,10 @@ namespace ServiceRestSMS.Controllers
                 LogMensaje log = new LogMensaje();
                 log.FECHA = DateTime.Now;
                 log.MENSAJE = e.Message;
-                log.ID_TIPOMENSAJE = 6;                
+                log.ID_TIPOMENSAJE = 6;
                 db.SaveChanges();
-                return Json(false);
-            }
-        }
-
-        public bool EnviarSM(string ArgMensaje, string ArgCarrier, string ArgTelefono, bool ArgEsControl)
-        {
-            MilDiasEntities db = new MilDiasEntities();
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://64.76.120.14:6064/minsaludsanjuan");
-            string responseText = "";
-            request.ContentType = "text/xml";
-            request.Method = "POST";
-
-            MTRequest MT = new MTRequest();
-            MT.Proveedor.Id = "minsaludsanjuan";
-            MT.Proveedor.Password = "mgs4ludsju4n";
-            MT.Servicio.Id = ArgCarrier;
-            MT.Servicio.ContentType = "0";
-            MT.Telefono.Msisdn = ArgTelefono;
-            MT.Contenido.Text = ArgMensaje;
-
-            var body = new StringWriter();
-            var serializer = new XmlSerializer(typeof(MTRequest));
-            serializer.Serialize(body, this);
-
-            byte[] postBytes = Encoding.UTF8.GetBytes(body.ToString());
-            request.ContentLength = postBytes.Length;
-            Stream requestStream = request.GetRequestStream();
-            requestStream.Write(postBytes, 0, postBytes.Length);
-            requestStream.Close();
-            using (var response = (HttpWebResponse)request.GetResponse())
-            {
-                var encoding = ASCIIEncoding.ASCII;
-                using (var reader = new StreamReader(response.GetResponseStream(), encoding))
-                {
-                    responseText = reader.ReadToEnd();
-                }
-
-                LogMensaje log = new LogMensaje();
-                log.FECHA = DateTime.Now;
-                log.MENSAJE = responseText;
-                if (ArgEsControl == true)
-                {
-                    log.ID_TIPOMENSAJE = 4;
-
-                    var serializer = new XmlSerializer(typeof(MTResponse));
-                    MTResponse MTResp = new MTResponse();
-                    using (TextReader readerXML = new StringReader(responseText))
-                    {
-                        MTResp = (MTResponse)serializer.Deserialize(readerXML);
-                    }
-                    LogMensajeControl logControl = new LogMensajeControl();
-                    logControl.FECHA = DateTime.Now;
-                    logControl.ID_INSTANCIA = ParamSMS.ID_Instancia;
-                    logControl.ID_TRANSACCION = int.Parse(MTResp.Transaccion.IdTran);
-                    logControl.ID_RESPUESTA = 3; //Por defecto ponemos que no contesto
-                    db.LogMensajeControl.Add(logControl);
-                    db.SaveChanges();
-                }
-                else
-                {
-                    log.ID_TIPOMENSAJE = 1;
-                }
-                db.LogMensaje.Add(log);
-                db.SaveChanges();
-            }
-            return true;
+                return false;
+            }           
         }
 
 
